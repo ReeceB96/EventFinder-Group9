43506b90c8aabb6f179181588bf3d5e847723262
Meredith Grimes <meredithagrimes@gmail.com>
2019-12-02 18:03:28 -0500

    Added nav bar and moved logo, added restaurant container back, added nav styling to css, added new js to code file and corrected spelling errors

--- code.js
+++ code.js
@@ -1,73 +1,56 @@
 var search_value = "";
 
-function searchFuntion() {
-
+function searchFunction() {
     var searchbtn = document.querySelector("button")
     var searchEl = document.querySelector("#search");
-
     searchEl.addEventListener("change", function (event) {
         event.preventDefault();
         var parentEl = this.parentElement;
         search_value = parentEl.querySelector("#search").value;
         if (search_value === "") {
             return;
-
         }
-
-        retriveEventData(search_value)
-
+    
+        retrieveEventData(search_value)
+        
     });
-
 }
-
-searchFuntion()
-
-function retriveEventData() {
-
+searchFunction()
+function retrieveEventData() {
     // This is my API key
     var APIKey = "42auTpFZzVkA9bQdsnU1TKcaCMoXIyTu";
-    city = search_value;
-
     // Here I'm building the URL we need to query the database
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&source=FrontGate Tickets,Ticketmaster&keyword=concert" + "&city=" + search_value + "&size=40" + "&apikey=" + APIKey;
-
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&source=FrontGate Tickets,Ticketmaster&keyword=concert" + "&city=" + search_value +"&stateCode=NC&radius=50&unit=miles&size=40" + "&apikey=" + APIKey;
+    var queryURL1 = "https://developers.zomato.com/api/v2.1/geocode?lat=35.2295&lon=-81.7492"
     $.ajax({
         url: queryURL,
         method: "GET",
-
+        
     })
         // Im store all of the retrieved data inside of an object called "response"
         .then(function (response) {
             console.log("Event-- ", response)
-
             var allEvents = response._embedded.events
-
             console.log(allEvents.length)
             for (var i = 0; i < allEvents.length; i++) {
-
                 var image = $("<img>");
                 image.addClass("eventImage");
-
                 // //set events information from the response 
                 var eventName = allEvents[i].name;
-                var eventImageURL = allEvents[i].images[i].url;
+                //var eventImageURL = allEvents[i].images[i].url;
                 var eventDate = allEvents[i].dates.start.localDate;
                 var eventTime = allEvents[i].dates.start.localTime;
                 var eventLocation = allEvents[i]._embedded.venues[0].name;
                 var eventCity = allEvents[i]._embedded.venues[0].city.name;
+                var eventLat = allEvents[i]._embedded.venues[0].location.latitude;
+                var eventLong = allEvents[i]._embedded.venues[0].location.longitude;
                 var eventState = allEvents[i]._embedded.venues[0].state.stateCode;
                 var eventTicket = allEvents[i].url;
-
                 //Set the date 
-                console.log("date ", eventDate)
-                var eventDay = moment(eventDate).format("ddd");
+                 var eventDay = moment(eventDate).format("ddd");
                 var eventDate = moment(eventDate).format("LL")
-
-                console.log(" time ", eventTime)
-
                 if (search_value === null) {
                     return;
-
                 } else {
                     var tableRow = $("<tr>");
                     tableRow.addClass("cityRow");
@@ -75,12 +58,11 @@ function retriveEventData() {
                     var tableTd2 = $("<td>");
                     var tableTd3 = $("<td>");
                     var tableTd4 = $("<td>");
-
+                    var tableTd5 = $("<td>");
                     var eventImageURL = response._embedded.events[i].images[i].url;
                     image.attr("src", eventImageURL);
                     tableTd1.append(image);
                     tableRow.append(tableTd1);
-
                     //create a h tag to append  each content
                     var hTag1 = $("<h6>");
                     hTag1.text(eventDate)
@@ -88,7 +70,6 @@ function retriveEventData() {
                     hTag2.text(eventDay + " : " + eventTime)
                     tableTd2.append(hTag1, hTag2);
                     tableRow.append(tableTd2);
-
                     //create a h tag to append  each content
                     var hTag1 = $("<h6>");
                     hTag1.text(eventName)
@@ -96,7 +77,6 @@ function retriveEventData() {
                     hTag2.text(eventLocation + " - " + eventCity + ", " + eventState)
                     tableTd3.append(hTag1, hTag2);
                     tableRow.append(tableTd3);
-
                     //add a button to the td 4
                     var mybtn = $("<button>");
                     var atag = $("<a>");
@@ -107,12 +87,52 @@ function retriveEventData() {
                     mybtn.append(atag);
                     tableTd4.append(mybtn);
                     tableRow.append(tableTd4);
-
                     $(".table").append(tableRow);
-
                 }
-
+                retrieveRestaurantData(eventLat, eventLong)
             }
         })
-
+}
+function retrieveRestaurantData(eventLat, eventLong) {
+    var queryURL1 = "https://developers.zomato.com/api/v2.1/geocode?lat=" + eventLat + "&lon=" + eventLong;
+    $.ajax({
+        url: queryURL1,
+        method: "GET",
+        "headers": {
+            "accept": "application/json",
+            "user-key": "01076abed27547fdb6b4bf0fb551be22"
+        }
+    })
+        // Im store all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+            var i = 0;
+            var result = response;
+            console.log("Restaurant-- ", result)
+            var eventRestaurant = result.link;
+            var eventRestName= result.location.title;
+            var eventRestCity= result.location.city_name;
+            var eventCuisine = result.popularity.top_cuisines
+            //add a new table
+            var tableRow = $("<tr>");
+            tableRow.addClass("cityRow2");
+            var Td = $("<td>");
+             //create a h tag to append  each content
+             var hTag1 = $("<h6>");
+             hTag1.text("Title: " + eventRestName)
+             var hTag2 = $("<h6>");
+             hTag2.text(eventRestCity + " - " + eventCuisine)
+             Td.append(hTag1, hTag2);
+             tableRow.append(Td);
+            //add a button to append to the td 
+            var mybtn = $("<button>");
+            var atag = $("<a>");
+            atag.attr("href", eventRestaurant)
+            mybtn.addClass("restaurantButton");
+            atag.text("View Restaurants: "  )
+            atag.attr('target', '_blank')
+            mybtn.append(atag);
+            Td.append(mybtn);
+            tableRow.append(Td);
+            $(".table2").append(tableRow)
+        })
 }
